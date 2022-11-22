f21a4b42156c1a7de96efad67fde20b3639552eb
evoxeed <nikita.gomozov@gmail.com>
2022-11-23 00:59:00 +0400

    start mobile

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.evoxed.iooan.app',
+  appName: 'feodal-mobile',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
